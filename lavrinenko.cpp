7c8bffc23d3513e4f74c92b08af73aa01d59861e
JanClodMamDral <lavrinenko.lesha34@mail.ru>
2026-09-12 21:17:36 +0700

    fix:убраны бесполезные строки по замечаниям ревью

--- lavrinenko.cpp
+++ lavrinenko.cpp
@@ -5,10 +5,7 @@
 double pyramidVolume(double a, double h){
     return a*a*h/3;
 }
-//fdfsfdgdfgfd
-//fdgdfgdfgdfgff
-//dfgfdgdfgfd
-//dfgdfgfd
+
 //Высота боковой грани
 double pyramidApothem(double a, double h){
     return sqrt(h*h+(a/2)*(a/2));
