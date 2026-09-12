a0a8b0a6c5b27164ab3d8dc00862ed6b78d2bb9f
JanClodMamDral <lavrinenko.lesha34@mail.ru>
2026-09-12 21:11:20 +0700

    Добавлены бесполезные строки для проверки

--- lavrinenko.cpp
+++ lavrinenko.cpp
@@ -5,7 +5,10 @@
 double pyramidVolume(double a, double h){
     return a*a*h/3;
 }
-
+//fdfsfdgdfgfd
+//fdgdfgdfgdfgff
+//dfgfdgdfgfd
+//dfgdfgfd
 //Высота боковой грани
 double pyramidApothem(double a, double h){
     return sqrt(h*h+(a/2)*(a/2));
